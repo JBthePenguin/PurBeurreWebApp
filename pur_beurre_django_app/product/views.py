c63f771ec8b2a6492425b137b5b48665714ffaad
JBthePenguin <jblab976@gmail.com>
2018-05-12 13:08:35 +0300

    Add index.html

--- pur_beurre_django_app/product/views.py
+++ pur_beurre_django_app/product/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    message = "Page d'accueil search_product.html"
-    return HttpResponse(message)
+    template = loader.get_template('product/index.html')
+    return HttpResponse(template.render(request=request))
 
 def products_list(request):
     message = "Page de résultat products_list.html"
